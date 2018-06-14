6a0304528a14a47708a7cca2dfa317c1b4000e9b
daiki.fujioka <daiki.fujioka@rakus.co.jp>
2018-06-14 16:52:52 +0900

    ymlファイルとpow.xmlを修正しました。注:現状ローカルDBにアクセスしています

---
+++ src/main/java/com/example/ecommerceD/EcommerceDApplication.java
@@ -0,0 +1,12 @@
+package com.example.ecommerceD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceDApplication.class, args);
+	}
+}
