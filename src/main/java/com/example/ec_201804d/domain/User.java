828ea13b5edcda087f7c0121ab6fccbc73cc935f
yuki.kakiki <yuki.kakiki@rakus.co.jp>
2018-06-18 12:20:30 +0900

    domainのuser作成

---
+++ src/main/java/com/example/ec_201804d/domain/User.java
@@ -0,0 +1,55 @@
+package com.example.ec_201804d.domain;
+
+public class User {
+	long id;
+	String name;
+	String email;
+	String password;
+	String zipCode;
+	String address;
+	String telephone;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	
+	
+}
