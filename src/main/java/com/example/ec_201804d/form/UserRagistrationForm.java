8ee23690cd6c3e0ea2bf9fe5548ab23e19b1edcf
hibiki.ono <hibiki.ono@rakus.co.jp>
2018-06-18 17:02:12 +0900

    ユーザフォームクラス作成

---
+++ src/main/java/com/example/ec_201804d/form/UserRagistrationForm.java
@@ -0,0 +1,68 @@
+package com.example.ec_201804d.form;
+
+
+/**
+ * @author hibiki.ono
+ *
+ *ユーザ登録情報のリクエストパラメータを格納しておくためのフォームクラス.
+ */
+public class UserRagistrationForm {
+
+	/**名前*/
+	private String name;
+	/**メールアドレス*/
+	private String email;
+	/**パスワード*/
+	private String password;
+	/**確認用パスワード*/
+	private String conPassword;
+	/**郵便番号*/
+	private String zipCode;
+	/**住所*/
+	private String address;
+	/**電話番号*/
+	private String telephone;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConPassword() {
+		return conPassword;
+	}
+	public void setConPassword(String conPassword) {
+		this.conPassword = conPassword;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+}
