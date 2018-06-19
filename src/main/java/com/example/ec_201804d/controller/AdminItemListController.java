773676182df937055b02cb516f043f9f310f5468
daiki.fujioka <daiki.fujioka@rakus.co.jp>
2018-06-19 13:41:41 +0900

    管理者の商品一覧表示と商品検索の機能を実装しました。

---
+++ src/main/java/com/example/ec_201804d/controller/AdminItemListController.java
@@ -0,0 +1,35 @@
+package com.example.ec_201804d.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.ec_201804d.domain.Item;
+import com.example.ec_201804d.repository.ItemRepository;
+
+@Controller
+@Transactional
+@RequestMapping(value="/adminItemList")
+public class AdminItemListController {
+	@Autowired
+	private ItemRepository repository;
+	
+	@RequestMapping
+	public String showItemListView(Model model) {
+		List<Item> itemList = repository.findAll();
+		model.addAttribute("items", itemList);
+		return "adminItemList";
+	}
+	
+	@RequestMapping(value="/index")
+	public String index(String keyword, Model model) {
+		model.addAttribute("keyword", keyword);
+		List<Item> filterdItemList = repository.findByWord(keyword);
+		model.addAttribute("items", filterdItemList);
+		return "adminItemList";
+	}
+}
