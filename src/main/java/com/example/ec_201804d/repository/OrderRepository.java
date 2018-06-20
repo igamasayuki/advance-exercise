7474a0ca01f14eaa1d588e5e67818be05dd4f068
takanorinoguchi <takanori.noguchi@rakus.co.jp>
2018-06-20 10:38:20 +0900

    注文詳細表示可能

--- src/main/java/com/example/ec_201804d/repository/OrderRepository.java
+++ src/main/java/com/example/ec_201804d/repository/OrderRepository.java
@@ -3,12 +3,16 @@ package com.example.ec_201804d.repository;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
+import com.example.ec_201804d.domain.AdminUser;
 import com.example.ec_201804d.domain.Order;
-
 
 /**
  * 注文一覧を表示するレポジトリ.
@@ -18,8 +22,8 @@ import com.example.ec_201804d.domain.Order;
  */
 @Repository
 public class OrderRepository {
-	
-	private static final RowMapper<Order> ORDER_ROW_MAPPER =(rs,i) ->{
+
+	private static final RowMapper<Order> ORDER_ROW_MAPPER = (rs, i) -> {
 		Order order = new Order();
 		order.setId(rs.getInt("id"));
 		order.setOrderNumber(rs.getString("order_number"));
@@ -32,22 +36,60 @@ public class OrderRepository {
 		order.setDeliveryZipCode(rs.getString("delivery_zip_code"));
 		order.setDeliveryAddress(rs.getString("delivery_address"));
 		order.setDeliveryTel(rs.getString("delivery_tel"));
-		
+
 		return order;
 	};
-	
+
 	@Autowired
 	NamedParameterJdbcTemplate template;
-	
+
 	/**
 	 * 全件検索を行うメソッド.
 	 * 
 	 * @return 注文のリストを返す
 	 */
-	public List<Order> findAll(){
-		String sql ="SELECT id,order_number,user_id,status,total_price,order_date,delivery_name,delivery_email,delivery_zip_code,delivery_address,delivery_tel FROM orders";
-		
+	public List<Order> findAll() {
+		String sql = "SELECT id,order_number,user_id,status,total_price,order_date,delivery_name,delivery_email,delivery_zip_code,delivery_address,delivery_tel FROM orders ORDER BY id";
 		List<Order> orderList = template.query(sql, ORDER_ROW_MAPPER);
 		return orderList;
+	}
+
+	/**
+	 * 注文詳細表示メソッド.
+	 * 
+	 * @param id
+	 *            ID
+	 * @return 注文情報を返す
+	 */
+	public Order load(Integer id) {
+		String sql = "SELECT id,order_number,user_id,status,total_price,order_date,delivery_name,delivery_email,delivery_zip_code,delivery_address,delivery_tel FROM orders WHERE id=:id";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		Order order = template.queryForObject(sql, param, ORDER_ROW_MAPPER);
+		return order;
+	}
+
+	public void insert(AdminUser adminUser) {
+		Integer currentMaxId = getMaxId();
+		if (currentMaxId == null) {
+			// テーブルにデータがない場合 -> IDを付与する
+			currentMaxId = 1;
+			adminUser.setId(currentMaxId);
+		} else {
+			adminUser.setId(currentMaxId + 1);
+		}
+		SqlParameterSource param = new BeanPropertySqlParameterSource(adminUser);
+		String sql = "insert into admin_users(id,name,email,password)values(:id,:name,:email,:password);";
+		template.update(sql, param);
+	}
+
+	public Integer getMaxId() {
+		try {
+			Integer maxId = template.queryForObject("SELECT MAX(id) FROM admin_users;", new MapSqlParameterSource(),
+					Integer.class);
+			return maxId;
+		} catch (DataAccessException e) {
+			// データが存在しない場合
+			return null;
+		}
 	}
 }
--- src/main/webapp/WEB-INF/views/orderList.jsp
+++ src/main/webapp/WEB-INF/views/orderList.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
 <!DOCTYPE html>
 <html>
@@ -11,45 +11,51 @@
 <title>注文一覧画面</title>
 </head>
 <body>
-<jsp:include page="adminHeader.jsp"/>
-<div align ="center">
-    <h2>注文一覧画面</h2>
-        
-        
-        <table border="1">
-<tr>
-<th>注文番号</th>
-<th>日付</th>
-<th>利用者名</th>
-<th>現在のステータス</th>
-<th>総計(税込)</th>
-</tr>
+	<jsp:include page="adminHeader.jsp" />
+	<div align="center">
+		<h2>注文一覧画面</h2>
+	</div>
+	<c:choose>
+		<c:when test="${orderList == null}">
+			<c:out value="商品がありません" />
+		</c:when>
+		<c:otherwise>
 
+			<table border="1" align="center">
+				<tr>
+					<th>注文番号</th>
+					<th>日付</th>
+					<th>利用者名</th>
+					<th>現在のステータス</th>
+					<th>総計(税込)</th>
+				</tr>
 
-<c:forEach var="orderList" items="${orderList}">
-<tr>
-<td>
-<a href="./orderDetail.jsp"><c:out value="${orderList.orderNumber}"/></a>
-</td>
-<td>
-<fmt:formatDate pattern="yyyy/MM/dd" value="${orderList.orderDate}"/>
-</td>
-<td>
-<c:out value="${orderList.deliveryName}"/>
-</td>
-<td>
-<c:out value="${orderList.status}"/>
-</td>
-<td>
-<fmt:formatNumber pattern="\###,###" value="${orderList.totalPrice}"/>
-</td>
-</tr>
-</c:forEach>
-</table>
-<br>     
-        <a href="administerMenu.jsp">メニューに戻れない</a>
+				<c:forEach var="orderList" items="${orderList}">
+					<tr>
+						<td><a
+							href="${pageContext.request.contextPath}/orderDetail?id=<c:out value= "${orderList.id}"/>">
+								<c:out value="${orderList.orderNumber}" />
+						</a></td>
+						<td><fmt:formatDate pattern="yyyy/MM/dd"
+								value="${orderList.orderDate}" /></td>
+						<td><c:out value="${orderList.deliveryName}" /></td>
+						<td><c:choose>
+								<c:when test="${orderList.status == 0}">未購入</c:when>
+								<c:when test="${orderList.status == 1}">未入金</c:when>
+								<c:when test="${orderList.status == 2}">入金済み</c:when>
+								<c:when test="${orderList.status == 3}">発送済み</c:when>
+								<c:when test="${orderList.status == 4}">キャンセル</c:when>
+							</c:choose></td>
+						<td><fmt:formatNumber pattern="\###,###"
+								value="${orderList.totalPrice}" /></td>
+					</tr>
+				</c:forEach>
+			</table>
+		</c:otherwise>
+	</c:choose>
 
-</div>
+	<a href="administerMenu.jsp">メニューに戻れない</a>
+
 
 </body>
 </html>
