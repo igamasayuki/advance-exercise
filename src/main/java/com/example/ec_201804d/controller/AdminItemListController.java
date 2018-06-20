6f12e609284a37cbfaeb5ea5125727f6da3496ad
daiki.fujioka <daiki.fujioka@rakus.co.jp>
2018-06-20 09:54:04 +0900

    javadocを追加しました。

--- src/main/java/com/example/ec_201804d/controller/AdminItemListController.java
+++ src/main/java/com/example/ec_201804d/controller/AdminItemListController.java
@@ -11,13 +11,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.ec_201804d.domain.Item;
 import com.example.ec_201804d.repository.ItemRepository;
 
+/**
+ * 管理者の商品一覧画面を表示するコントローラ.
+ *
+ * @author daiki.fujioka
+ *
+ */
 @Controller
 @Transactional
 @RequestMapping(value="/adminItemList")
 public class AdminItemListController {
+	/** 商品DBを扱うリポジトリ */
 	@Autowired
 	private ItemRepository repository;
 	
+	/**
+	 * 管理者の商品一覧画面を表示する.
+	 * @param model リクエストスコープ
+	 * @return 管理者の商品一覧画面
+	 */
 	@RequestMapping
 	public String showItemListView(Model model) {
 		List<Item> itemList = repository.findAll();
@@ -26,6 +38,12 @@ public class AdminItemListController {
 		return "adminItemList";
 	}
 	
+	/**
+	 * 商品の文字列検索を行い、商品一覧画面を表示する.
+	 * @param keyword 検索文字列
+	 * @param model リクエストスコープ
+	 * @return 管理者の商品一覧画面
+	 */
 	@RequestMapping(value="/index")
 	public String index(String keyword, Model model) {
 		model.addAttribute("keyword", keyword);
