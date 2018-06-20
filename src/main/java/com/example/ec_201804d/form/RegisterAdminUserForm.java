da088d7886ed9c54ca1889c0bbafbf3ae3b009f0
yuki.kakiki <yuki.kakiki@rakus.co.jp>
2018-06-20 10:21:58 +0900

    コメント追加

--- src/main/java/com/example/ec_201804d/form/RegisterAdminUserForm.java
+++ src/main/java/com/example/ec_201804d/form/RegisterAdminUserForm.java
@@ -3,17 +3,25 @@ package com.example.ec_201804d.form;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
+/**
+ * @author kakiki
+ *管理者登録フォーム.
+ */
 public class RegisterAdminUserForm {
+	/**名前*/
 	@NotEmpty(message="値を入力してください")
 	private String name;
 	
+	/**eメール*/
 	@Email( message = "メールアドレスの形式ではありません" )
 	@NotEmpty( message = "値を入力してください")
 	private String email;
 	
+	/**パスワード*/
 	@NotEmpty( message = "値を入力してください")
 	private String password;
 	
+	/**確認用パスワード*/
 	@NotEmpty( message = "値を入力してください")
 	private String checkPassword;
 	
