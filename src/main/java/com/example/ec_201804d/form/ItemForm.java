9c66033a867abd28e7bbc6c1354200bcb85986c0
hibiki.ono <hibiki.ono@rakus.co.jp>
2018-06-25 15:39:33 +0900

    管理者側の商品検索404エラーを解消しました

--- src/main/java/com/example/ec_201804d/form/ItemForm.java
+++ src/main/java/com/example/ec_201804d/form/ItemForm.java
@@ -12,25 +12,23 @@ import javax.validation.constraints.Pattern;
 public class ItemForm {
 
 	/** 個数 */
-	@NotNull(message="個数を入力してください")
-	@Pattern(regexp = "^[1-9][0-9]*+$",message="自然数で入力してください")
+	@NotNull(message = "個数を入力してください")
+	@Pattern(regexp = "^[1-9][0-9]*+$", message = "自然数で入力してください")
 	private String quantity;
 	/** 商品のID */
 	private Long id;
 
-//	/** 以下setter,getter */
-//	public Integer getQuantity() {
-//		return quantity;
-//	}
-//
-//	public void setQuantity(Integer quantity) {
-//		this.quantity = quantity;
-//	}
+	// /** 以下setter,getter */
+	// public Integer getQuantity() {
+	// return quantity;
+	// }
+	//
+	// public void setQuantity(Integer quantity) {
+	// this.quantity = quantity;
+	// }
 	public Integer getIntQuantity() {
 		return Integer.parseInt(quantity);
 	}
-
-
 
 	public String getQuantity() {
 		return quantity;
@@ -40,17 +38,12 @@ public class ItemForm {
 		this.quantity = quantity;
 	}
 
-
-
 	public Long getId() {
 		return id;
 	}
 
-
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 
 }
